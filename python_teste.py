887326e1baf0047f52b4ff219fb6342ad404e4cc
Innova Academy <rerevelation12@gmail.com>
2023-11-30 15:52:06 -0300

    meu primeiro código

---
+++ python_teste.py
@@ -0,0 +1,9 @@
+# Código com erro
+numeros = [1, 2, 3, 4, 5]
+
+# Calcula a média dos números
+soma = sum(numeros) #Soma +
+media = soma / len(numeros)
+
+# Exibe a média
+print(f"A média é: {media}")
